066de5e87fb713a1d073deee5b8f62de15e1363d
Hardik Malhotra <hardikmalhotra150804@gmail.com>
2025-05-07 16:00:51 +0530

    userschema helper

---
+++ backend/src/index.ts
@@ -0,0 +1,29 @@
+import bodyParser from "body-parser";
+import compression from "compression";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import express from "express";
+import http from "http";
+import mongoose from "mongoose";
+
+const app = express();
+
+app.use(
+  cors({
+    credentials: true,
+  })
+);
+
+app.use(compression());
+app.use(cookieParser());
+app.use(bodyParser.json());
+
+const server = http.createServer(app);
+
+server.listen(3000, () => {
+  console.log(`Server running on http://localhost:3000`);
+});
+
+mongoose.Promise = Promise;
+mongoose.connect(process.env.mongodb_url);
+mongoose.connection.on("error", (error: Error) => console.log(error));

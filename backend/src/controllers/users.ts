d38f691554eef6a3ea64079ad49614a85a4cfb49
Hardik Malhotra <hardikmalhotra150804@gmail.com>
2025-05-09 13:56:02 +0530

    middleware

---
+++ backend/src/controllers/users.ts
@@ -0,0 +1,50 @@
+import { Request, Response } from "express";
+import { deleteUserById, getUser, getUserById } from "../db/Users";
+
+export const getAllUsers = async (
+  req: Request,
+  res: Response
+): Promise<any> => {
+  try {
+    const users = await getUser();
+
+    return res.status(200).json(users);
+  } catch (error) {
+    console.log("GET_ALL_USERS_ERROR" + error);
+    return res.status(500);
+  }
+};
+
+export const deleteUser = async (req: Request, res: Response): Promise<any> => {
+  try {
+    const { id } = req.params;
+
+    const deletedUser = await deleteUserById(id);
+
+    return res.status(200).json(deletedUser);
+  } catch (error) {
+    console.log("DELETE_USERS_ERROR" + error);
+    return res.status(500);
+  }
+};
+
+export const updateUser = async (req: Request, res: Response): Promise<any> => {
+  try {
+    const { id } = req.params;
+    const { username } = req.body;
+
+    if (!username) {
+      return res.status(400);
+    }
+
+    const user = await getUserById(id);
+
+    user.username = username;
+    await user.save();
+
+    return res.status(200).json(user);
+  } catch (error) {
+    console.log("UPDATE_USERS_ERROR" + error);
+    return res.status(500);
+  }
+};
---
+++ backend/src/middlewares/index.ts
@@ -0,0 +1,59 @@
+import { getUserBySessionToken } from "db/Users";
+import { NextFunction, Request, Response } from "express";
+import { get, merge } from "lodash";
+
+export const isOwner = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const { id } = req.params;
+    const currentUserId = get(req, "identity._id") as string;
+
+    if (!currentUserId) {
+      res.status(403);
+      return;
+    }
+
+    if (currentUserId.toString() !== id) {
+      res.status(403);
+      return;
+    }
+
+    next();
+  } catch (error) {
+    console.log("MIDDLEWARE_ERROR" + error);
+    res.status(500).json({ message: "Internal Server Error" });
+    return;
+  }
+};
+
+export const isAuthenticated = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const sessionToken = req.cookies["AUTH"];
+    if (!sessionToken) {
+      res.status(401).json({ message: "Unauthorzied" });
+      return;
+    }
+
+    const existingUser = await getUserBySessionToken(sessionToken);
+
+    if (!existingUser) {
+      res.status(404).json({ message: "User doesnt exist" });
+      return;
+    }
+
+    merge(req, { identity: existingUser });
+
+    return next();
+  } catch (error) {
+    console.log("MIDDLEWARE_ERROR" + error);
+    res.status(500).json({ message: "Internal Server Error" });
+    return;
+  }
+};
